7deecf5d892371c21711f13f380c59e3c60aea69
ducndthe153148 <ducndthe153148@fpt.edu.vn>
2022-09-08 15:21:32 +0700

    first commit

---
+++ demo_metamask/android/app/src/main/kotlin/com/example/demo_metamask/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.demo_metamask
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ demo_metamask/lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:demo_metamask/utils/routes.dart';
+import 'package:flutter/material.dart';
+
+import 'pages/login_page.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: MyRoutes.loginRoute,
+      routes: {
+        MyRoutes.loginRoute: (context) => const LoginPage(),
+      },
+    );
+  }
+}
